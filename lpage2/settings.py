34210dfcdc0b0fadc29c2db85549b346c4fb35e5
jagger909 <jagger909@gmail.com>
2017-08-11 21:43:17 +0300

    КВОС деплой на хероку

--- lpage2/settings.py
+++ lpage2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j^^%o8lelxvkem_pet3*9tn37*&*^lv4s6z)ghxorwz@fjj2+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
