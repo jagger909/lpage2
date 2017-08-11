e628317da71a3a658606324a9e98f3a5450ef59c
jagger909 <jagger909@gmail.com>
2017-08-11 23:42:32 +0300

    КВОС деплой на хероку. edit settings

--- lpage2/settings.py
+++ lpage2/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'j^^%o8lelxvkem_pet3*9tn37*&*^lv4s6z)ghxorwz@fjj2+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
